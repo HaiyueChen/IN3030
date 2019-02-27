31f20656fc7d5a95ea3438efbb2478783f7f626f
HaiyueChen <chenhaiyue2010@hotmail.com>
2019-02-27 09:16:10 +0100

    blabla

--- oblig2/Parallel.java
+++ oblig2/Parallel.java
@@ -3,5 +3,36 @@
  */
 public class Parallel {
     
-    
+    public static double[][] transpose(double[][] matrix){
+        double[][] trans = new double[matrix.length][matrix.length];
+
+        return trans;
+    }
+
+    public static double[][] multiply_normal(double[][] a, double[][] b){
+        double[][] product = new double[a.length][a.length];
+
+
+        return product;
+    }
+
+    public static double[][] multiply_a_transposed(double[][] a, double[][] b){
+        double[][] product = new double[a.length][a.length];
+        double[][] trans_a = transpose(a);
+
+
+        return product;
+    }
+
+    public static double[][] multiply_b_transposed(double[][] a, double[][] b){
+        double[][] product = new double[a.length][a.length];
+        double[][] trans_b = transpose(b);
+
+
+        return product;
+    }
+
+
+
 }
+
