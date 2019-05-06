9393933e4f5c70ad5aae31193f5e2da38f16e346
HaiyueChenm <chenhaiyue2010@hotmail.com>
2019-05-06 12:31:38 +0100

    done

--- oblig5/Test.java
+++ oblig5/Test.java
@@ -5,18 +5,13 @@ import java.util.Arrays;
  */
 public class Test {
 
-    static int num_point = 30;
+    static int num_point = 1000;
+
     public static void main(String[] args) {
-        if (args.length == 1) {
-            int count = Integer.valueOf(args[0]);
-            Oblig5 test = new Oblig5(count);
-            IntList test_list = test.seqMethod();
-            TegnUt t = new TegnUt(test, test_list);    
-        }
-        else{
-            Oblig5 test = new Oblig5(num_point);
-            IntList test_list = test.seqMethod();
-            TegnUt t = new TegnUt(test, test_list);
-        }
+        Oblig5 test = new Oblig5(num_point);
+        IntList seq_list = test.seqMethod();
+        System.out.println(Arrays.toString(seq_list.data) + "\n\n");
+        IntList para_list = test.paraMethod(8);
+        System.out.println(Arrays.toString(para_list.data) + "\n\n");
     }
 }
